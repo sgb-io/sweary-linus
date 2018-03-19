c687d401038106226763ef05b5e1147a03049540
Sam Brown <sam@sgb.io>
2018-03-19 13:07:19 +0000

    Refactor functional method

--- javascript/functional/functional.js
+++ javascript/functional/functional.js
@@ -1,6 +1,9 @@
 module.exports = function main(insults, stopWords) {
     const words = []
     const rawWords = insults.split(' ')
+    const wordCounts = {}
+    const unsortedWordCounts = []
+    let topWords
 
     rawWords.forEach((rawWord) => {
         // Normalize (down-case) every word
@@ -21,7 +24,6 @@ module.exports = function main(insults, stopWords) {
     })
 
     // Count the occurrences of all words
-    const wordCounts = {}
     words.forEach((word) => {
         if (wordCounts.hasOwnProperty(word)) {
             wordCounts[word]++
@@ -30,7 +32,7 @@ module.exports = function main(insults, stopWords) {
 
         wordCounts[word] = 1
     })
-    const unsortedWordCounts = []
+    
     Object.keys(wordCounts).forEach((word) => {
         unsortedWordCounts.push({
             word,
@@ -39,14 +41,18 @@ module.exports = function main(insults, stopWords) {
     })
 
     // Finally, print out the 25 most common words in order
-    const sortedWordCounts = unsortedWordCounts.sort((a, b) => {
-        return b.count - a.count
-    })
-    const topWords = sortedWordCounts.slice(0, 25)
+    topWords = unsortedWordCounts
+        .sort((a, b) => {
+            return b.count - a.count
+        })
+        .slice(0, 25)
+    
     topWords.forEach((topWord, index) => {
-        const rank = index + 1
-        console.log(`#${rank} - "${topWord.word}", ${topWord.count} occurrences`)
-    })
+            const rank = index + 1
+            console.log(
+                `#${rank} - "${topWord.word}", ${topWord.count} occurrences`
+            )
+        })
 
     return topWords
 }
