3a603c69f94b243638b93ecb16d160793da7bc8c
Sam Brown <sam@sgb.io>
2018-03-19 13:11:21 +0000

    Codestyle

--- javascript/functional/functional.js
+++ javascript/functional/functional.js
@@ -3,7 +3,7 @@ module.exports = function main(insults, stopWords) {
     const rawWords = insults.split(' ')
     const wordCounts = {}
     const unsortedWordCounts = []
-    let topWords
+    let topWords = []
 
     rawWords.forEach((rawWord) => {
         // Normalize (down-case) every word
@@ -26,7 +26,7 @@ module.exports = function main(insults, stopWords) {
     // Count the occurrences of all words
     words.forEach((word) => {
         if (wordCounts.hasOwnProperty(word)) {
-            wordCounts[word]++
+            wordCounts[word] += 1
             return
         }
 
@@ -36,23 +36,21 @@ module.exports = function main(insults, stopWords) {
     Object.keys(wordCounts).forEach((word) => {
         unsortedWordCounts.push({
             word,
-            count: wordCounts[word]
+            count: wordCounts[word],
         })
     })
 
     // Finally, print out the 25 most common words in order
     topWords = unsortedWordCounts
-        .sort((a, b) => {
-            return b.count - a.count
-        })
+        .sort((a, b) => (b.count - a.count))
         .slice(0, 25)
-    
+
     topWords.forEach((topWord, index) => {
-            const rank = index + 1
-            console.log(
-                `#${rank} - "${topWord.word}", ${topWord.count} occurrences`
-            )
-        })
+        const rank = index + 1
+        console.log(
+            `#${rank} - "${topWord.word}", ${topWord.count} occurrences`
+        )
+    })
 
     return topWords
 }
