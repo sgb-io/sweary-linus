f825426d7c4701b406eefc32a54c12df0a8d83bf
Sam Brown <sam@sgb.io>
2018-06-12 17:25:12 +0100

    Add dart implementation starting point

---
+++ dartlang/README.md
@@ -0,0 +1,13 @@
+# Dart
+
+## Requirements
+
+Dart 2 SDK via https://www.dartlang.org/tools/sdk#install (dev channel is required for v2 at time of writing)
+
+Note: if you already installed Dart 1.x via homebrew, use `brew upgrade dart --devel`.
+
+## Run me
+
+e.g. from this dir:
+
+`dart main.dart ../insults.txt ../stopwords.txt`
---
+++ dartlang/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+Future readLines(String filepath) async {
+  return new File(filepath)
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(const LineSplitter());
+}
+
+void main(List<String> arguments) async {
+  if (arguments.length != 2) {
+    stderr.writeln(
+        'Not enough arguments. Example usage: dart main.dart ../insults.txt ../stopwords.txt');
+  }
+
+  var lines = await readLines(arguments[0]);
+  await for (var line in lines) {
+    stdout.writeln(line);
+  }
+}
