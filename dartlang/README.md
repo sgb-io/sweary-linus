1f1c932cdc9e0d6b22fe80c3e8f4898c3bcd5fcf
Sam Brown <sam@sgb.io>
2018-06-13 00:12:38 +0100

    Dart (#41)
    Dart

---
+++ dartlang/README.md
@@ -0,0 +1,21 @@
+# Dart
+
+## Requirements
+
+Dart 2 SDK via https://www.dartlang.org/tools/sdk#install (dev channel is required for v2 at time of writing)
+
+Note: if you already installed Dart 1.x via homebrew, use `brew upgrade dart --devel`.
+
+Install deps via `pub get`.
+
+## Run me
+
+e.g. from this dir:
+
+`dart main.dart ../insults.txt ../stopwords.txt`
+
+## Run my tests
+
+From this directory (filepaths to insults & stopwords hardcoded)
+
+`dart test.dart`
---
+++ dartlang/main.dart
@@ -0,0 +1,122 @@
+import 'dart:async';
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:io';
+
+Future main(List<String> arguments) async {
+  if (arguments.length != 2) {
+    stderr.writeln(
+        'Not enough arguments. Example usage: dart main.dart ../insults.txt ../stopwords.txt');
+  }
+
+  // Import insults & stopwords into lists of words
+  List<String> insults = await getInsults(arguments[0]);
+  List<String> stopwords = await getStopwords(arguments[1]);
+
+  // Reduce insults to letters-only, lowercase & no stopwords
+  List<String> cleanInsults = getCleanedInsults(insults, stopwords);
+
+  // Count the occurences of each word, sort by most occurences
+  LinkedHashMap<String, int> countedWords = countWords(cleanInsults);
+  LinkedHashMap sortedWordCounts = sortWordCounts(countedWords);
+
+  // Print the top 25 (and return so that we can test)
+  List wordCountKeys = sortedWordCounts.keys.toList(growable: false);
+  List<String> outputLines = [];
+  for (var i = 0; i < 25; i++) {
+    String word = wordCountKeys[i];
+    int occurences = sortedWordCounts[word];
+    String line = '#${i+1} - "${word}", ${occurences} occurrences';
+    stdout.writeln(line);
+    outputLines.add(line);
+  }
+
+  return outputLines;
+}
+
+LinkedHashMap<String, int> countWords(List<String> words) {
+  LinkedHashMap<String, int> wordCounts = new LinkedHashMap();
+
+  for (var word in words) {
+    if (!wordCounts.containsKey(word)) {
+      wordCounts[word] = 1;
+    } else {
+      wordCounts[word] += 1;
+    }
+  }
+
+  return wordCounts;
+}
+
+List<String> getCleanedInsults(List<String> insults, List<String> stopwords) {
+  List<String> cleanInsults = [];
+
+  for (var rawInsult in insults) {
+    String asLower = rawInsult.toLowerCase();
+    String asLetters = removeNonLetters(asLower);
+
+    if (asLetters == '') {
+      continue;
+    }
+
+    if (stopwords.contains(asLower) || stopwords.contains(asLetters)) {
+      continue;
+    }
+
+    cleanInsults.add(asLetters);
+  }
+
+  return cleanInsults;
+}
+
+Future getInsults(String filepath) async {
+  List<String> words = [];
+  var insultLines = await readLines(filepath);
+  await for (var line in insultLines) {
+    for (var word in line.split(' ')) {
+      words.add(word);
+    }
+  }
+
+  return words;
+}
+
+Future getStopwords(String filepath) async {
+  List<String> words = [];
+  var stopwordLines = await readLines(filepath);
+  await for (var line in stopwordLines) {
+    words.add(line);
+  }
+
+  return words;
+}
+
+Future readLines(String filepath) async {
+  return new File(filepath)
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(const LineSplitter());
+}
+
+String removeNonLetters(String word) {
+  return word.replaceAll(new RegExp(r'[^a-z]'), '');
+}
+
+LinkedHashMap sortWordCounts(LinkedHashMap wordCounts) {
+  LinkedHashMap resMap = new LinkedHashMap();
+  List mapKeys = wordCounts.keys.toList(growable: false);
+
+  // Sort by value (occurences), then key (word, implicitly alphabetical)
+  mapKeys.sort((a, b) {
+    if (wordCounts[b] == wordCounts[a]) {
+      return a.compareTo(b);
+    } else {
+      return wordCounts[b] - wordCounts[a];
+    }
+  });
+  for (var key in mapKeys) {
+    resMap[key] = wordCounts[key];
+  }
+
+  return resMap;
+}
